a044413680c9bb22a3909031960425232445d6e0
DoodieBot <140124349+DoodieBot@users.noreply.github.com>
2024-11-18 09:50:55 +1100

    copy and paste is what i did

--- app.py
+++ app.py
@@ -1,18 +1,42 @@
 from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+db = SQLAlchemy(app)
 
-def get_db_connection():
-    conn = sqlite3.connect('movie_collection.db')
-    return conn
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    director = db.Column(db.String(100))
+    year = db.Column(db.Integer)
+    rating = db.Column(db.Float)
+
+
+with app.app_context():
+    db.create_all()
+
 
 @app.route('/')
 def index():
-  conn = get_db_connection()
-    movies = conn.execute('SELECT * FROM movies').fetchall()
-    conn.close()
+    movies = Movie.query.all()
     return render_template('index.html', movies=movies)
 
+@app.route('/add', methods=['GET', 'POST'])
+def add_movie():
+    if request.method == 'POST':
+        new_movie = Movie(
+            title=request.form['title'],
+            director=request.form['director'],
+            year=int(request.form['year']),
+            rating=float(request.form['rating'])
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add.html')
+    
 if __name__ == '__main__':
     app.run(debug=True)
